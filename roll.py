26a26270f32af611b97ce253c7f221c658acf98f
Cody Phrampus <cphrampu@purdue.edu>
2018-03-15 21:29:15 -0500

    Use suggestion from Player's Handbook to make sure scores give at least a +5

--- roll.py
+++ roll.py
@@ -11,3 +11,15 @@ def roll_stat():
 # use the roll_stat method to return 6 stats
 def roll_stats():
     return [stat for stat in [roll_stat() for _ in range(6)]]
+
+
+# returns the modifier given a score, ranges from -5 for 1 and +5 for 20
+def get_mod(score):
+    return (score - 10) // 2
+
+# roll stats and make sure the the sum of the mods is at least 5, the sum of the standard array
+def roll_stats_min_mod():
+    stats = roll_stats()
+    while sum([get_mod(i) for i in stats]) < 5:
+        stats = roll_stats()
+    return sorted(stats, reverse=True)
