e3090c3b85d6f0be2993ec493528355313f94a2e
Cody Phrampus <cphrampu@purdue.edu>
2018-03-15 23:06:16 -0500

    Create a random character given the weights in the yaml files

--- creator.py
+++ creator.py
@@ -1,6 +1,7 @@
 import os
 
 import yaml
+import numpy.random
 
 
 
@@ -8,6 +9,60 @@ import yaml
 races = yaml.load(open("Data/races.yaml", "r"))
 
 
+# given a list of values with weights, randomly pick one
+def random_from(items):
+    # make a value weight tuple for each item, them zip them so there is a value array and a weight array
+    values, weights = zip(*[(value, items[value]["weight"]) for value in items])
+    total = sum(weights)
+    probs = [weight / total for weight in weights]
+    return numpy.random.choice(values, p=probs)
+
+
+def create():
+    # create an array of values to keep track of choices
+    attr = []
+
+    # create object to return as created character info
+    character = {}
+
+    # choose a random race and add it to the list
+    race = random_from(yaml.load(open("Data/races.yaml")))
+    attr += [race]
+
+    # choose a random subrace of our chosen race and add it to the list
+    subrace = random_from(yaml.load(open("Data/Gen/{}/subraces.yaml".format(*attr))))
+    attr += [subrace]
+
+    # choose a random background for our race/subrace choices
+    background = random_from(yaml.load(open("Data/Gen/{}/{}/backgrounds.yaml".format(*attr))))
+
+    # save choices to character
+    character["background"] = background
+    character["race"] = race
+
+    # not all races have subraces
+    character["subrace"] = subrace if subrace != "" else None
+
+    cl = random_from(yaml.load(open("Data/Gen/{}/{}/classes.yaml".format(*attr))))
+
+    # save class choice
+    attr += [cl]
+
+    # choose random archetype for our choices thus far
+    archetype = random_from(yaml.load(open("Data/Gen/{}/{}/{}/archetypes.yaml".format(*attr))))
+
+    # print out our choices
+    print(background, race + ("({})".format(subrace) if subrace != "" else ""), cl + "({})".format(archetype))
+
+    # save class and archetype to character
+    character["class"] = cl
+    character["archetype"] = archetype
+
+    return character
+
+
+# TODO modify to break down into yamls including stats, mods, etc.
+# TODO refactor
 # generates basic yaml files, should only be used for creating all files
 # does not deal with modifiers, proficiencies, or anything above weighting everything the same
 def gen_yamls():
